0180c9974d354f968e8d61ca4caebf5f939e6b84
PFranciscoRojas <francisco.rojas.diaz@outlook.com>
2023-08-14 22:54:25 -0500

    delete comments

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
         System.out.println("Bienvenido al juego RPG");
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite el nombre de su personaje: ");
