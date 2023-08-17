934d4228810528fa965f50bd21ff7fe9c915e6fc
JEYBAN37 <jeyban37@gmail.com>
2023-08-16 22:34:03 -0500

    Add Class Inventory and Enum Arms Completed V.1

---
+++ src/inventory/Inventory.java
@@ -0,0 +1,44 @@
+package src.inventory;
+
+import enums.Arms;
+import enums.NameMonster;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventory {
+    private List<String> armsInventory;
+    private static Inventory instance;
+    private Inventory() {
+    }
+
+    public static synchronized Inventory getInstance() {
+        if (instance == null) {
+            instance = new Inventory();
+        }
+        return instance;
+    }
+
+    public void addItem(Arms arms) {
+        armsInventory.add(arms.getName());
+    }
+
+    public void removeItem(Arms arms) {
+        armsInventory.remove(arms.getName());
+    }
+
+    public List<String> getItems() {
+        return armsInventory;
+    }
+
+    public void AllObjectsList() {
+
+        for (String i : armsInventory) {
+                System.out.println(i);
+        }
+
+    }
+
+
+
+}
